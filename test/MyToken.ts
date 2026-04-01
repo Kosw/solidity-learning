937c1abc97ecea2f678e7d96a8b239bb5b222edb
Kosw <swko4512@gmail.com>
2026-04-02 02:20:57 +0900

    mytoken phase 1 - basic state

---
+++ test/MyToken.ts
@@ -0,0 +1,38 @@
+import hre from "hardhat";
+import { expect } from "chai";
+import { MyToken } from "../typechain-types";
+import { HardhatEthersSigner } from "@nomicfoundation/hardhat-ethers/signers";
+
+describe("mytoken deploy", () => {
+    let myTokenC: MyToken;
+    let signers: HardhatEthersSigner[];
+    before("should deploy", async () => {
+        signers = await hre.ethers.getSigners();
+        myTokenC = await hre.ethers.deployContract("MyToken", [
+            "MyToken",
+            "MT",
+            18
+        ]);
+    });
+
+    it("should check name", async () => {
+        expect(await myTokenC.name()).to.equal("MyToken");
+    });
+
+    it("should check symbol", async () => {
+        expect(await myTokenC.symbol()).to.equal("MT");
+    });
+
+    it("should check decimals", async () => {
+        expect(await myTokenC.decimals()).to.equal(18);
+    });
+
+    it("should return 0 total supply", async () => {
+        expect(await myTokenC.totalSupply()).to.equal(0);
+    });
+
+    it("should return 0 balance for signer", async () => {
+        const signer0 = signers[0];
+        expect(await myTokenC.balanceOf(signer0)).to.equal(0);
+    });
+});
